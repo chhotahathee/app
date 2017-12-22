6617bf9a2b22047829fc6a3c26ecfbbb5224d26a
chhotahathee <34777159+chhotahathee@users.noreply.github.com>
2017-12-23 00:06:38 +0530

    Add files via upload

---
+++ src/com/chhotahathee/Defaults.java
@@ -0,0 +1,11 @@
+package com.chhotahathee;
+
+public class Defaults
+{
+  
+public static final String APPLICATION_ID = "C2177F74-51D8-CD42-FFF5-0911B2570500";
+public static final String API_KEY = "EC31BAB8-43EA-9788-FF64-A30006361200";
+public static final String SERVER_URL = "https://api.backendless.com";
+    
+}
+                                            
